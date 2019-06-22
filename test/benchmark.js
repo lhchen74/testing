61f0a9570213ec5021b44b004c5ab54d2e2d45b3
Babb Chen <11435@SDC.SERCOMM.com>
2019-06-22 18:24:44 +0800

    add benchmark

---
+++ test/benchmark.js
@@ -0,0 +1,44 @@
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite;
+const {
+    num1,
+    num2
+} = require('../src/fn')
+
+suite.add('parseInt', () => {
+        num1('123456')
+    })
+    .add('Number', () => {
+        num2('123456')
+    })
+    .on('cycle', (event) => {
+        console.log(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    .run({async: true})
+
+
+
+
+
+// // add tests
+// suite.add('RegExp#test', function() {
+//   /o/.test('Hello World!');
+// })
+// .add('String#indexOf', function() {
+//   'Hello World!'.indexOf('o') > -1;
+// })
+// .add('String#match', function() {
+//   !!'Hello World!'.match(/o/);
+// })
+// // add listeners
+// .on('cycle', function(event) {
+//   console.log(String(event.target));
+// })
+// .on('complete', function() {
+//   console.log('Fastest is ' + this.filter('fastest').map('name'));
+// })
+// // run async
+// .run({ 'async': true });
